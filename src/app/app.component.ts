8dcbb824b697aaa2b3c11f4d5bed14c3618c95fb
fernando.junior <fernando.junior@informata.com.br>
2022-11-17 18:31:10 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from './service.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'heroes-marvel';
+
+  constructor(private service: ServiceService) { }
+
+
+  ngOnInit(): void {
+    this.teste()
+  }
+
+  teste() {
+    this.service.getMusic().subscribe(
+      (result) => {
+        console.log(result)
+       },
+      (error) => { },
+    )
+  }
+
+
 }
---
+++ src/app/service.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServiceService {
+
+  timeStamp = '1'
+  privateKey = '5bccdc8a0b4b460863d4d687bfff960243762793'
+  publicKey = '3579a66ac3ace7cef9476ce2d5f8030e'
+  hash = '5a224723a56c79734d956fbb5ecb1fd6'
+
+  constructor(private http: HttpClient) { }
+
+  getMusic(): Observable<any> {
+    return this.http.get(`https://gateway.marvel.com:443/v1/public/characters?ts=1&orderBy=name&limit=10&apikey=3579a66ac3ace7cef9476ce2d5f8030e&hash=${this.hash}`)
+  }
+
+}
